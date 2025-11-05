57a71e6019956bc2ebd2deaf337f2f0a78b04e55
umha23 <umha2305@gmail.com>
2025-11-05 16:05:59 +0700

    ubah icons logo 2

--- service-worker.js
+++ service-worker.js
@@ -7,8 +7,8 @@ const urlsToCache = [
   `${BASE_URL}offline.html`,
   `${BASE_URL}assets/style.css`,
   `${BASE_URL}manifest.json`,
-  `${BASE_URL}icons/icon-logo eventraze 192x192.png`,
-  `${BASE_URL}icons/icon-logo eventraze 512x512.png`,
+  `${BASE_URL}icons/logoeventraze192x192.png`,
+  `${BASE_URL}icons/logoeventraze512x512.png`,
 ];
 
 // Install Service Worker & simpan file ke cache
